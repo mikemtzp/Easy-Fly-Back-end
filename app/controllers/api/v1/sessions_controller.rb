b68a6c836894eba35a115f5240e0ad4d4cf5ee2d
worashf <uppert83@gmail.com>
2022-10-24 14:06:09 +0300

    Correct issue token method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < Devise::SessionsController
   def create
     @user = User.find_by(email: login_params[:email])
     if @user&.valid_password?(login_params[:password])
-      @token = @user.generate_token
+      @token = issue_token(@user)
       render json: { token: @token, username: @user.name, user_role: @user.role, email: @user.email }, status: :ok
     else
       render json: { error_message: 'Login failed!', details: 'Email or password is invalid' },
