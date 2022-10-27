914c435f5bebd0f2785c2b9422d756303d8ad342
masangana <alexmasangana@gmail.com>
2022-10-28 00:04:31 +0300

    Create registration spec

---
+++ spec/requests/api/v1/registrations_spec.rb
@@ -0,0 +1,37 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/registrations', type: :request do
+
+  path '/api/v1/signup/sign_up' do
+
+    get('new registration') do
+      tags 'Registrations'
+      response(200, 'successful') do
+
+        consumes 'application/json'
+        
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            email: { type: :string },
+            name: { type: :string },
+            password: { type: :string },
+            photo: { type: :string }
+          },
+
+          required: [ 'email', 'name', 'password', 'photo']
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+end
