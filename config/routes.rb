319357340600e5c1e029959a5d508dc1667c8b18
masangana <alexmasangana@gmail.com>
2022-10-27 21:03:00 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  
   scope module: 'api', path: "api" do 
 
   scope :v1 do 
