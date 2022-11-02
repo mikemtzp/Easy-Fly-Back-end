ab957fcd4b490b69df5e0af3c23d4e0a177e15dd
worashf <uppert83@gmail.com>
2022-11-02 23:22:33 +0300

    Change secrect key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
-class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound do
-    render json: { error: 'Resource not found!' }, status: :not_found
-  end
 
-  rescue_from ActionController::RoutingError do
-    render json: { error: 'bad route' }, status: :not_found
-  end
+class ApplicationController < ActionController::API
+  respond_to :json
+  before_action :update_allowed_parameters, if: :devise_controller?
+
 
   def secret_key
-    Rails.application.secrets.secret_key_base
+   Rails.application.secret_key_base
   end
 
   def token
@@ -16,8 +13,9 @@ class ApplicationController < ActionController::API
   end
 
   def issue_token(user)
-    JWT.encode({ user_id: user.id, exp: 60.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
+    JWT.encode({ id:user.id, exp: 1.month.from_now.to_i }, Rails.application.secret_key_base)
   end
+  
 
   def decoded_token
     JWT.decode(token, secret_key)[0]
@@ -28,10 +26,26 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
-    @current_user ||= User.find_by(id: decoded_token['user_id'])
+    @current_user ||= User.find_by(id: decoded_token['id'])
   end
 
   def logged_in?
     current_user.present?
   end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { error: 'Resource not found!' }, status: :not_found
+  end
+  
+   rescue_from ActionController::RoutingError do
+      render json: { error: 'bad route' }, status: :not_found
+  end
+
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name,:username, :email, :password) }
+  end
+
 end
